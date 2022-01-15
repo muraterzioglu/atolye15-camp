16352b8914c3fdafcc3dde3c3ea7cce8202b9bd9
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2022-01-15 23:31:04 +0300

    Progress on Avatars before 12:00am
    Letters got from Firstname & Lastname, options for circle and square with parameters.

--- src/avatars/avatars.controller.ts
+++ src/avatars/avatars.controller.ts
@@ -1,16 +1,44 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Query,
+  Get,
+  Response,
+  StreamableFile,
+  Header,
+} from '@nestjs/common';
 import { AvatarsService } from './avatars.service';
+import { Readable } from 'stream';
+
+/*
+TODO: Make Colors Random with contrast
+TODO: Make logos by size
+TODO: Connect Database and Check that if it's already make!
+*/
 
 @Controller('avatars')
 export class AvatarsController {
   constructor(private readonly avatarsService: AvatarsService) {}
 
-  @Get('/avatar?')
+  @Get('?')
+  @Header('Content-type', 'image/png')
   avatar(
+    @Response({ passthrough: true }) res,
     @Query('name') name: string,
     @Query('size') size: number,
     @Query('shape') shape: string,
-  ): string {
-    return this.avatarsService.avatar(name, size, shape);
+  ): StreamableFile {
+    const buffer = this.avatarsService.avatar(name, size, shape);
+
+    const readableInstanceStream = new Readable({
+      read() {
+        this.push(buffer);
+        this.push(null);
+      },
+    });
+    res.set({
+      'Content-Type': 'image/png',
+      'Content-Disposition': 'inline; filename="avatar.png"',
+    });
+    return new StreamableFile(readableInstanceStream);
   }
 }
--- src/avatars/avatars.service.ts
+++ src/avatars/avatars.service.ts
@@ -1,8 +1,44 @@
+// import * as fs from 'fs';
 import { Injectable } from '@nestjs/common';
+import { createCanvas } from 'canvas';
 
 @Injectable()
 export class AvatarsService {
-  avatar(name: string, size: number, shape: string): string {
-    return `Name: ${name} Size: ${size} Shape: ${shape}`;
+  avatar(name: string, size: number, shape: string): Buffer {
+    // Get Letters
+    const named = name.split(' ');
+    const first = named[0].charAt(0);
+    const last = named[named.length - 1].charAt(0);
+    const text = first + '' + last;
+
+    // Canvas Settings
+    const width = 420;
+    const height = 420;
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+
+    if (shape == 'circle') {
+      context.arc(210, 210, 210, 0, Math.PI * 2, false);
+      context.fillStyle = 'yellow';
+      context.fill();
+      context.font = 'bold 120% Calibri';
+      context.textAlign = 'center';
+
+      context.fillStyle = '#000000';
+      context.fillText(text, 210, 210 + 30);
+
+      return canvas.toBuffer('image/png');
+    } else if (shape == 'square') {
+      context.rect(0, 0, 420, 420);
+      context.fillStyle = '#000';
+      context.fill();
+      context.font = 'bold 120% Calibri';
+      context.textAlign = 'center';
+
+      context.fillStyle = '#FFF';
+      context.fillText(text, 210, 240);
+
+      return canvas.toBuffer('image/png');
+    }
   }
 }
